16cebebeea1fae3c25702c15b9bd08eb06d7a18f
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-05-13 17:43:30 +0800

    add comments and explanations of the code

--- geoiter.py
+++ geoiter.py
@@ -7,6 +7,7 @@ class geoiter:
     
     def __init__(self, bounds, resolution, zoom = None):
 
+        # verify if the geoinformation is valid
         if abs(bounds[0]) >= 90 or abs(bounds[2]) >= 90:
             raise Exception("Latitude bigger than 90 or smaller than -90")
         elif abs(bounds[1]) >= 180 or abs(bounds[3]) >= 180:
@@ -14,13 +15,15 @@ class geoiter:
 
         self.bound = bounds
 
+        # verify if the resolutions are valid
         if resolution[0] < 0 or resolution[1] < 0:
             raise Exception("Resolution smaller than zero")
 
         self.resolution = resolution
 
+        # when zoom level is not provided, the algorithm will calculate the minimum zoom level needed
         if zoom is None:
-            self.zoom = self._minimum_resolution(bounds, resolution)
+            self.zoom = self._minimum_zoom(bounds, resolution)
         elif zoom < 0 or zoom > 19:
             raise Exception("Zoom level bigger than 19 or smaller than 0")
         else:
@@ -30,12 +33,16 @@ class geoiter:
         self.pre_computed_imgs = self._map_tiler(bounds[0], bounds[1], bounds[2], bounds[3], self.zoom, resolution)
         
     def __iter__(self):
+
+        # initialize the iterator
         self.count = 0
         return self
     
     def __next__(self):
         if self.count < len(self.pre_computed_imgs):
             current_block = self.pre_computed_imgs[self.count]
+
+            # fetch the image with precomputed tile and crop information
             img = self._img_with_given_resolution(current_block[0], current_block[1], self.zoom)
             self.count += 1
             
@@ -43,6 +50,7 @@ class geoiter:
         else:
             raise StopIteration
         
+    # convert from lat/lng to tiles
     def _geo_converter(self, lat_deg, lon_deg, zoom):
         lat_rad = math.radians(lat_deg)
         n = 2.0 ** zoom
@@ -50,6 +58,7 @@ class geoiter:
         ytile = int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2.0 * n)
         return (xtile, ytile)
         
+    # check if images with the resolution could be processed under given boundary and zoom level
     def _check_resolution(self, xtile1, ytile1, xtile2, ytile2, zoom, resolution):
     
         len_x = (abs(xtile1 - xtile2) + 1) * 256
@@ -67,7 +76,8 @@ class geoiter:
 
         return math.floor(multiplier_x), math.floor(multiplier_y)
 
-    def _minimum_resolution(self, boundary, resolution):
+    # return the minimum zoom level required to satisfy the given resolution
+    def _minimum_zoom(self, boundary, resolution):
 
         for i in range(20):
 
@@ -82,14 +92,20 @@ class geoiter:
     
         raise Exception("Impossible to return an image with given resolution under the defined boundary.")
     
+    # find the tiles that 'wrap' the image and how to crop it to get the image
     def _find_tile(self, top_left, bottom_right, outer_top_left, outer_bottom_right):
     
+        # x, y refers to the resolution of the image
         x = bottom_right[0] - top_left[0]
         y = bottom_right[1] - top_left[1]
 
+        # find the distance of current images' top left corner to the boundary's top left corner
+        # the result is in the unit of tiles
         distance_from_top_x = top_left[0] // 256
         distance_from_top_y = top_left[1] // 256
 
+        # find the distance of current images' bottom right corner to the boundary's bottom_right corner
+        # the result is in the unit of tiles
         distance_from_bottom_x = outer_bottom_right[0] - math.ceil(bottom_right[0] / 256) + 1
         distance_from_bottom_y = outer_bottom_right[1] - math.ceil(bottom_right[1] / 256) + 1
 
@@ -101,44 +117,64 @@ class geoiter:
 
         return tiles, crop
     
+    # core function that returns the pre-computed information of each image
     def _map_tiler(self, lat1, lng1, lat2, lng2, zoom, resolution):
     
+        # gets the boundary within tile system
         xtile1, ytile1 = self._geo_converter(lat1, lng1, zoom)
         xtile2, ytile2 = self._geo_converter(lat2, lng2, zoom)
         
+        # checks whehter the xtile and ytile is correct
         self._check_tile_convertion_integrity(xtile1, ytile1, zoom)
         self._check_tile_convertion_integrity(xtile2, ytile2, zoom)
 
+        # mp_x represents how many pieces of the image will be on the x-axis
+        # mp_y represents how many pieces of the image will be on the y-axis
+        # the total number of images returned is mp_x * mp_y
         mp_x, mp_y = self._check_resolution(xtile1, ytile1, xtile2, ytile2, zoom, resolution)
 
+        # upper_left represents the top left corner of the boundary
+        # bottom_right represents the bottom right corner of the boundary
         upper_left = (min(xtile1, xtile2), min(ytile1, ytile2))
         bottom_right = (max(xtile1, xtile2), max(ytile1, ytile2))
 
+        # temp_upper represents the top_left corner of an instance of the image within the boundary
+        # in the algorithm, cutting starts from top_left corner. Therefore, the relative position of
+        # the first image is [0, 0]. Notice that the unit is in pixels rather than tile
         temp_upper = [0, 0]
         temp_bottom = [resolution[0], resolution[1]]
         pre_computed_imgs = []
 
+        # the for loop iterates through every image that is going to be returned
         for i in range(mp_x):
 
             temp_upper[1] = 0
             temp_bottom[1] = resolution[1]
 
             for t in range(mp_y):
-                tiles, crop = self._find_tile(temp_upper, temp_bottom, upper_left, bottom_right)
-                pre_computed_imgs.append([tiles, crop]) 
 
+                # the _find_tile function returns two important results:
+                # 'tiles' refers to the tiles that "wrap" the image in
+                # 'crop' refers to the how the tiles should be cropped in order to get the resulting image 
+                tiles, crop = self._find_tile(temp_upper, temp_bottom, upper_left, bottom_right)
+                pre_computed_imgs.append([tiles, crop])
+
+                # the relative position of the image moves downwards for the next image
                 temp_upper[1] += resolution[1]
                 temp_bottom[1] += resolution[1]
 
+            # the relative position of the image moves leftwards for the next image
             temp_upper[0] += resolution[0]
             temp_bottom[0] += resolution[0]
 
         return pre_computed_imgs
     
+    # return the image with pre-computed information
     def _img_with_given_resolution(self, tiles, crop, zoom):
     
         responses = []
 
+        # fetch all the tiles necessary to crop the image
         for i in range(tiles[0], tiles[2] + 1):
             for t in range(tiles[1], tiles[3] + 1):
                 responses.append(requests.get('https://a.tiles.mapbox.com/v3/nickponline.g7642h2a/%s/%s/%s.png' % (zoom, i, t)))
@@ -156,6 +192,7 @@ class geoiter:
         y_offset = 0
         count = 0
 
+        # first assemble all the tiles
         for i in range(tiles[2] - tiles[0] + 1):
             for t in range(tiles[3] - tiles[1] + 1):
                 new_im.paste(imgs[count], (x_offset, y_offset))
@@ -164,10 +201,12 @@ class geoiter:
             y_offset = 0
             x_offset += 256
 
+        # crop the image
         new_im = new_im.crop(crop)
 
         return new_im
     
+    # check if the tile information is valid
     def _check_tile_convertion_integrity(self, xtile, ytile, zoom):
         
         tile_size = 2 ** zoom - 1
--- test_geoiter.py
+++ test_geoiter.py
@@ -3,6 +3,7 @@ from geoiter import geoiter
 
 class GeoiterTestCase(unittest.TestCase):
 
+    # test resolution higher than 256 * 256
     def test_256_300(self):
         bounds = (80, 170, -80, -170)
         resolution = (256, 300)
@@ -12,6 +13,7 @@ class GeoiterTestCase(unittest.TestCase):
 
         self.assertEqual(len(test.pre_computed_imgs), 2)
 
+    # test resolution lower than 256 * 256
     def test_60_40(self):
         bounds = (80, 170, -80, -170)
         resolution = (60, 40)
@@ -21,6 +23,7 @@ class GeoiterTestCase(unittest.TestCase):
 
         self.assertEqual(len(test.pre_computed_imgs), 96)
 
+    # test if invalid lat/lng is correctly captured
     def test_lat_lng_exception(self):
         bounds = (90, 180, -90, -180)
         resolution = (60, 40)
@@ -31,6 +34,7 @@ class GeoiterTestCase(unittest.TestCase):
 
         self.assertTrue("Latitude bigger than 90 or smaller than -90" in str(context.exception))
 
+    # test if resolution too high under current boundary and zoom level is correctly captured
     def test_resolution_exception(self):
         bounds = (80, 170, -80, -170)
         resolution = (1280, 720)
@@ -41,6 +45,7 @@ class GeoiterTestCase(unittest.TestCase):
 
         self.assertTrue("Unable to meet the set resolution requirement" in str(context.exception))
 
+    # test if invalid zoom level is correctly captured
     def test_zoom_exception(self):
         bounds = (80, 170, -80, -170)
         resolution = (60, 40)
@@ -51,6 +56,7 @@ class GeoiterTestCase(unittest.TestCase):
 
         self.assertTrue("Zoom level bigger than 19 or smaller than 0" in str(context.exception))
 
+    # test automatic calculation of the minimum zoom level
     def test_without_zoom(self):
         bounds = (80, 170, -80, -170)
         resolution = (60, 40)
@@ -59,6 +65,7 @@ class GeoiterTestCase(unittest.TestCase):
 
         self.assertEqual(len(test.pre_computed_imgs), 96)
 
+    # test if resolution too higher under current boundary is correctly captured
     def test_without_zoom_exception(self):
         bounds = (31.2304, 121.4737, 31.2305, 121.4738)
         resolution = (1280, 760)
